0ce9663c55dd62ab34c5ada035500756dd41d0dd
Alexman2505 <alexman2505@gmail.com>
2023-04-02 22:47:38 +0300

    Бот работает и исправно. Но он возвращает только последнюю домашку, даже если я ставлю время на 0. Попробую в следующей коммите переделать его так, чтобы он возвращал мне все домашки, когда я ставлю отсчет на ноль

--- homework.py
+++ homework.py
@@ -3,7 +3,6 @@ import os
 import sys
 import time
 from http import HTTPStatus
-from typing import Any, Optional, Union, cast
 
 import requests
 import telegram
@@ -87,14 +86,14 @@ class HomeworksAreAbsentException(Exception):
     pass
 
 
-def check_tokens() -> Union[bool, None]:
+def check_tokens() -> None:
     """Проверяет доступность переменных окружения.
 
-    Возвращает "истину", если все переменные окружения доступны.
-    Если какой-то переменной в окружении нет -
+    Ничего не возвращает, если все переменные окружения доступны.
+    Но если какой-то переменной в окружении нет -
     выбрасывает исключение.
     """
-    TOKENS: dict[str, Optional[str]] = {
+    TOKENS = {
         'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
         'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
         'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID,
@@ -108,10 +107,9 @@ def check_tokens() -> Union[bool, None]:
             f'Потеряны переменные окружения из '
             f'этого списка {FORGET_TOKENS}'
         )
-    return True
 
 
-def get_api_answer(timestamp: int) -> Union[dict, None]:
+def get_api_answer(timestamp) -> dict:
     """Делает запрос к API ЯП Домашка с временной меткой timestamp.
 
     Возвращает ответ от API ЯП Домашка,
@@ -136,39 +134,37 @@ def get_api_answer(timestamp: int) -> Union[dict, None]:
     return response.json()
 
 
-def check_response(response: dict) -> Union[bool, None]:
+def check_response(response: dict) -> None:
     """Проверяет ответ API ЯП Домашка.
 
     Проверяет содержимое словаря "response" на соответствие некоторым тестам.
-    Возвращает "истину", если данные в словаре валидны.
-    А если API ЯП Домашка вернул словарь с невалидными данными -
-    выбрасываются некоторые "встроенные" исключения и одно собственное.
+    Ничего не возвращает. Но если API ЯП Домашка вернул словарь с
+    невалидными данными - выбрасываются некоторые "встроенные"
+    исключения и одно собственное.
     """
     if not isinstance(response, dict):
-        raise TypeError('Ответ API ЯП не является словарем')
+        raise TypeError('Ответ API ЯП не является словарем, а должен')
     if 'homeworks' not in response:
-        raise KeyError('В ответе API ЯП нет ключа homeworks')
+        raise KeyError(
+            'В ответе API ЯП нет ключа homeworks, а это первый из '
+            'двух ключей, в нем должен лежат список всех домашек.'
+        )
     if 'current_date' not in response:
-        raise KeyError('В ответе API ЯП нет ключа current_date')
+        raise KeyError(
+            'В ответе API ЯП нет ключа current_date, в нем лежит время'
+        )
     if not isinstance(response.get('homeworks'), list):
-        raise TypeError('Ответ API ЯП не в виде списка')
-    current_date: Optional[Union[int, None]] = response.get('current_date')
-    if (
-        current_date is None
-        or not isinstance(current_date, int)
-        or current_date < 0
-        or current_date > int(time.time())
-    ):
-        raise TypeError('API ЯП вернул некорректное время')
-    homeworks: Optional[list[Any]] = response.get('homeworks')
-    if homeworks is None or len(homeworks) == 0:
+        raise TypeError('Ответ API ЯП не в виде структуры данных "список".')
+    if not isinstance(response.get('current_date'), int):
+        raise TypeError('API ЯП вернул время запроса в неправильном формате')
+    homeworks = response['homeworks']
+    if len(homeworks) == 0:
         raise HomeworksAreAbsentException(
             'Ответ API ЯП пришел без данных о новых домашках'
         )
-    return True
 
 
-def parse_status(homework: dict) -> Union[str, None]:
+def parse_status(homework: dict) -> str:
     """Извлекает статусы конкретной домашней работы.
 
     Проверяет название и статус домашней работы на "валидность".
@@ -179,14 +175,14 @@ def parse_status(homework: dict) -> Union[str, None]:
         raise KeyError(
             'Ответ от API ЯП домашка не содержит ключа "homework_name".'
         )
+    if 'status' not in homework:
+        raise KeyError('Ответ от API ЯП домашка не содержит ключа "status".')
     if homework.get('status') not in HOMEWORK_VERDICTS:
         raise HomeworkStatusIsUncorrectException(
             'У домашней работы некорректный статус.'
         )
-    homework_name = homework.get('homework_name')
-    verdict: Optional[str] = HOMEWORK_VERDICTS.get(
-        cast(str, homework.get('status'))
-    )
+    homework_name = homework['homework_name']
+    verdict = HOMEWORK_VERDICTS[homework['status']]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -212,12 +208,12 @@ def main():
     while True:
         try:
             response = get_api_answer(timestamp)
-            if check_response(response):
-                homework = response.get('homeworks')[0]
-                message = parse_status(homework)
-                logger.info(f'Есть обновление {message}')
-                send_message(bot, message)
-            timestamp = response.get('current_date', timestamp)
+            check_response(response)
+            homework = response.get('homeworks')[0]
+            message = parse_status(homework)
+            logger.info(f'Есть обновление {message}')
+            send_message(bot, message)
+            timestamp = response.get('current_date')
         except HomeworksAreAbsentException as deb:
             logger.debug(deb)
         except (
